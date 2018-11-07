3134e9f9b523a7ee02e16c6f72e7e0acbe38d10c
Richard Smith <ricsmith1223@gmail.com>
2018-11-07 11:52:47 -0500

    made change to file

--- manifests/file_http.pp
+++ manifests/file_http.pp
@@ -1,4 +1,4 @@
 file { '/tmp/README.md':
-	source => 'https://raww.githubusercontent.com/puppetlabs/puppet/master/README.MD',
+	source => 'https://raw.githubusercontent.com/puppetlabs/puppet/master/README.MD',
 
 }
